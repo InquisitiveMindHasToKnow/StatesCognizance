11ba0e8f5d6262f4b345968b45f70fc4bd51d435
InquisitiveMindHasToKnow <omarraymond@pursuit.org>
2020-04-30 17:59:36 -0400

    Added textview for state area and border states to DisplayInfoFragment

--- app/src/main/java/org/ohmstheresistance/statescognizance/data/StateInfo.kt
+++ app/src/main/java/org/ohmstheresistance/statescognizance/data/StateInfo.kt
@@ -1,4 +1,4 @@
 package org.ohmstheresistance.statescognizance.data
 
-data class StateInfo(val name: String, val abbreviation: String, val flag: String,
+data class StateInfo(val name: String, val abbreviation: String, val flag: String, val admissionToStatehood: String,
                      val capital: String, val nickname: String, val population: Int)
