8583ac680e978a373e078f49296eb1866c002748
InquisitiveMindHasToKnow <omarraymond@pursuit.org>
2020-05-14 21:35:01 -0400

    Added an image of the US Map colored with the color of the flag set to show up 2.9 seconds after app launch on HomePageFragment.

--- app/src/main/java/org/ohmstheresistance/statescognizance/fragments/HomePageFragment.kt
+++ app/src/main/java/org/ohmstheresistance/statescognizance/fragments/HomePageFragment.kt
@@ -1,12 +1,14 @@
 package org.ohmstheresistance.statescognizance.fragments
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.home_page_fragment.*
 
 import org.ohmstheresistance.statescognizance.R
 import org.ohmstheresistance.statescognizance.databinding.HomePageFragmentBinding
@@ -14,12 +16,24 @@ import org.ohmstheresistance.statescognizance.databinding.HomePageFragmentBindin
 class HomePageFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-
         val binding = DataBindingUtil.inflate<HomePageFragmentBinding>(inflater, R.layout.home_page_fragment, container, false)
+
+        setHomePageImage()
 
         binding.homePageButton.setOnClickListener { findNavController().navigate(HomePageFragmentDirections.actionHomePageFragmentToStatesFragment()) }
 
         return binding.root
     }
 
+    private fun setHomePageImage(){
+        val homepageImageHandler = Handler()
+        val homepageImageRunnable = Runnable {
+            run {
+
+                home_page_imageview.visibility = View.VISIBLE
+                home_page_image.visibility = View.GONE
+            }
+        }
+        homepageImageHandler.postDelayed(homepageImageRunnable, 2900)
+    }
 }
